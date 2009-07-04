eef319280679d34845fd2290f8a46d2a8373ce1e
Hugo Doria <hugo@archlinux.org>
2009-07-04 00:00:34 -0300

    Adding some comments to the ssh module

--- hntool/ssh.py
+++ hntool/ssh.py
@@ -29,40 +29,46 @@ class rule:
 		ssh_conf_file = '/etc/ssh/sshd_config'
 		
 		if os.path.isfile(ssh_conf_file):
-			fp = open(ssh_conf_file,'r')
+			fp = open(ssh_conf_file,'r') 
 			lines = [x.strip('\n') for x in fp.readlines()]			
 			
+			# Checking if SSH is using the default port
 			if 'Port 22' in lines or '#Port 22' in lines:					
 				check_results[1].append('SSH is using the default port')
 			else:
 				check_results[0].append('SSH is not using the default port')
 				
-				
+			# Checking if the Root Login is allowed	
 			if 'PermitRootLogin yes' in lines or '#PermitRootLogin' in lines:
 				check_results[1].append('Root access allowed!')
 			else:
 				check_results[0].append('Root access is not allowed!')
           
+			# Checking if SSH is using protocol v2 (recommended)
 			if 'Protocol 2' not in lines:    
 				check_results[1].append('SSH is not using protocol v2')
 			else:
 				check_results[0].append('SSH is using protocol v2')
 				
+			# Checking if empty password are allowed (shouldn't)
 			if 'PermitEmptyPasswords yes' in lines:
 				check_results[2].append('Empty passwords are allowed')
 			else:
 				check_results[0].append('Empty passwords are not allowed')
         
+			# Checking if X11 Forward is allowed (shouldn't)
 			if 'X11Forwarding yes' in lines:
 				check_results[1].append('X11 forward is allowed')
 			else:
 				check_results[0].append('X11 forward is not allowed')
         
+			# Checking if SSH allow TCP Forward (shouldn't)
 			if 'AllowTcpForwarding yes' in lines:
 				check_results[1].append('TCP forwarding is allowed')
 			else:
 				check_results[0].append('TCP forwarding is not allowed')
 				
+			# Closing the sshd_config file
 			fp.close()
 					
 		return check_results
