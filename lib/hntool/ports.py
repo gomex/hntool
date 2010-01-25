8501063953d0ffb5cfb7654042392978bc7d6312
Rafael Goncalves Martins <rafael@rafaelmartins.eng.br>
2010-01-25 20:40:53 -0300

    Using lsof instead of netstat in the Ports module

--- lib/hntool/ports.py
+++ lib/hntool/ports.py
@@ -18,46 +18,49 @@
 #
 
 import os
+import re
 
 class rule:
-	def short_name(self):
-		return "port checks"
-	def long_name(self):
-		return "Checks for open ports"
-	def __init__(self, options):
-		pass
-	def analyze(self, options):
-		check_results = [[],[],[],[],[]]
-		
-		# Checking for old files in /tmp
-		netstat_results = os.popen('LC_ALL="en_US" netstat -tulpan | grep LISTEN')
-		netstat_results = [res for res in netstat_results.readlines()]
+    def short_name(self):
+        return "port checks"
+    def long_name(self):
+        return "Checks for open ports"
+    def __init__(self, options):
+        pass
+    def analyze(self, options):
+        check_results = [[],[],[],[],[]]
+        out = os.popen('LC_ALL=C lsof -i -nP').read()
+        services = {}
+        # the regex need some improvement
+        for i in re.finditer(r'([a-z0-9_-]+).*:([0-9]+) \(LISTEN\)', out):
+            service_name = i.group(1)
+            service_port = i.group(2)
+            if service_name not in services:
+                services[service_name] = [service_port]
+            elif service_port not in services[service_name]:
+                services[service_name].append(service_port)
+        if len(services) > 0:
+            for service in services:
+                if len(services[service]) == 1:
+                    tmp_msg = 'port "' + services[service][0] + '"'
+                else:
+                    tmp_msg = 'ports "' + '" and "'.join(services[service]) \
+                        + '"'
+                check_results[4].append('Service "' + service + '" using ' \
+                    + tmp_msg + ' found')
+        else:
+            check_results[0].append("There's no ports opened")
 
-		if len(netstat_results) > 0:
-			lines = [ filter(None,netstat_results[indexc].split(' ')) \
-		                for indexc, item in enumerate(netstat_results) ]
+        device_list = ['/dev/null', '/dev/tty', '/dev/console']
+        exec_device = False
+        for device in device_list:
+            if os.path.lexists(device):
+                if os.access(device ,os.X_OK):
+                    check_results[3].append('Found device "' + device + '" with executable rights')
+                    exec_device = True
+        if not exec_device:
+            check_results[1].append('Devices without executable rights')
 
-			for indexc, item in enumerate(lines):
-				port = lines[indexc][3].split(':')[1]
-				service = lines[indexc][6].split('/')
-				if len(service) == 1: service = service[0]
-				else: service = service[1].rstrip()
-				check_results[4].append('Service "' + service + '" ' + \
-																'using port "' + port + '" found')
-
-		else:
-			check_results[0].append("There's no ports opened")
-
-		device_list = ['/dev/null', '/dev/tty', '/dev/console']
-		exec_device = False
-		for device in device_list:
-			if os.path.lexists(device):
-				if os.access(device ,os.X_OK):
-					check_results[3].append('Found device "' + device + '" with executable rights')
-					exec_device = True
-		if not exec_device:
-			check_results[1].append('Devices without executable rights')
-
-		return check_results
-	def type(self):
-		return "config"
+        return check_results
+    def type(self):
+        return "config"
